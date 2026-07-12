11f20879f20014d3bcc75d64cb51353e6c1f3c20
SANN GROUP <sannndevops@gmail.com>
2026-07-12 12:07:46 +0700

    Upload public/sw.js

---
+++ public/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'githubupps-cache-v1';
+const urlsToCache = ['/', '/manifest.json', '/icon-192x192.png', '/icon-512x512.png'];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
